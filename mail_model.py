d51f2f155b355e16a99118fb703d7f3188c64e63
Grigoriy Smorodnikov <zhozhinc@zhozhinc.com>
2020-09-19 22:25:50 +0300

    mail_attempts

--- mail_model.py
+++ mail_model.py
@@ -1,6 +1,5 @@
-import smtplib
-import os
-import mimetypes
+import smtplib, ssl, os, mimetypes, yagmail
+
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
@@ -55,7 +54,8 @@ def send_email(addr_to: str, msg_subj: str, msg_text: str, file: str = None) -> 
     if file:
         attach_file(msg, file)
 
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    #server.ehlo()
     #server.starttls()
     #server.set_debuglevel(True)
     server.login(addr_from, password)
@@ -66,4 +66,68 @@ def send_email(addr_to: str, msg_subj: str, msg_text: str, file: str = None) -> 
     #     return False
 
 
+def send_email_20():
+    #import smtplib, ssl
+
+    port = 25  # For starttls
+    smtp_server = "localhost"
+    sender_email = from_addr
+    receiver_email = to_addr
+    password = password
+    message = """\
+    Subject: Hi there
+
+    This message is sent from Python."""
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        #server.helo()
+        server.ehlo()  # Can be omitted
+        #server.starttls(context=context)
+        #server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+
+def send_email_30():
+    server = smtplib.SMTP('smtp.gmail.com', 25)
+    #server.connect("smtp.gmail.com", 465)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(login, password)
+    text = msg.as_string()
+    server.sendmail(from_addr, to_addr, text)
+    server.quit()
+
+
+def send_mail_40():
+    #def sendMail(to, fro, subject, text, files=[],server="localhost"):
+    #assert type(to)==list
+    #assert type(files)==list
+
+
+    msg = MIMEMultipart()
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    #msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = "hello"
+
+    msg.attach( MIMEText("hey") )
+
+    #for file in files:
+    #    part = MIMEBase('application', "octet-stream")
+    #    part.set_payload( open(file,"rb").read() )
+    #    Encoders.encode_base64(part)
+    #    part.add_header('Content-Disposition', 'attachment; filename="%s"'
+    #                   % os.path.basename(file))
+    #    msg.attach(part)
+
+    smtp = smtplib.SMTP("localhost")
+    smtp.sendmail(from_addr, to_addr, msg.as_string() )
+    smtp.close()
+
+send_mail_40()
+#send_email_30()
+#send_email_20()
 #send_email("zhozhpost@gmail.com", "subj", "text")
